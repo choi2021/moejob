904ec8a5af806287f0b126d95857cf4335a91cd3
Youngjun Choi <juni2004c@gmail.com>
2022-11-28 21:06:45 +0900

    chore: 이전파일 정리

--- context/httpContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext, useState } from 'react';
-
-export const HttpContext = createContext();
-export function HttpContextProvider({ children }) {
-  return <HttpContext.Provider>{children}</HttpContext.Provider>;
-}
